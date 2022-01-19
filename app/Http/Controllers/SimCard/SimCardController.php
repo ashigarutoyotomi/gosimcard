803313628a9d36887407617b87eb0272ac71a98f
abe codeman <abecodeman@gmail.com>
2022-01-19 13:54:46 +0600

    simcards search and filter ready

--- app/Http/Controllers/SimCard/SimCardController.php
+++ app/Http/Controllers/SimCard/SimCardController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\SimCard;
+
 use App\Http\Controllers\Controller;
 use App\Domains\SimCard\Gateways\SimCardGateway;
 use App\Http\Requests\SimCard\SimCardRequest;
@@ -14,39 +15,71 @@ use App\Domains\SimCard\Models\SimCard;
 
 class SimCardController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $simcards = SimCard::all();
-        return $simcards;
+        $validated = $request->validate([
+            'filters' => 'required|array'
+        ]);
+        $filters = $request->filters;
+        if ($filters['status'] != null && $filters['start_created_date'] == null && $filters['end_created_date'] == null) {
+            $simCards = SimCard::where('status', $filters['status'])->get();
+            return $simCards;
+        } elseif ($filters['status'] != null && $filters['start_created_date'] != null && $filters['end_created_date'] == null) {
+            $simCards = SimCard::where('status', $filters['status'])
+                ->where('created_at', '>=', $filters['start_created_date'])->get();
+            return $simCards;
+        } elseif ($filters['status'] == null && $filters['start_created_date'] != null && $filters['end_created_date'] != null) {
+            $simCards = SimCard::where('created_at', '>=', $filters['start_created_date'])
+                ->where('created_at', '<=', $filters['end_created_at'])->get();
+            return $simCards;
+        } elseif ($filters['status'] != null && $filters['start_created_date'] != null && $filters['end_created_date'] != null) {
+            $simCards = SimCard::where('status', $filters['status'])->where('created_at', '>=', $filters['start_created_date'])->where('created_at', '<=', $filters['end_created_date'])->get();
+            return $simCards;
+        }
+
+        // $simcards = SimCard::all();
+        // return $simcards;
     }
-    public function show($simCardId){
+    public function show($simCardId)
+    {
         $simcard = SimCard::find($simCardId);
-        abort_unless((bool)$simcard,404,'simcard not found');        
+        abort_unless((bool)$simcard, 404, 'simcard not found');
         return $simcard;
     }
-    public function store(Request $request){
+    public function store(Request $request)
+    {
         $validated = $request->validate([
-            'number'=>'required|string',
-            'status'=>'required',
-            'user_id'=>'required',
-            'days'=>'required',
+            'number' => 'required|string',
+            'status' => 'required',
+            'user_id' => 'required',
+            'days' => 'required',
         ]);
-        $data = new CreateSimCardData(['number'=>$request->number,
-        'status'=>(int)$request->status,
-        'days'=>(int)$request->days,
-        'user_id'=>(int)$request->user_id
+        $data = new CreateSimCardData([
+            'number' => $request->number,
+            'status' => (int)$request->status,
+            'days' => (int)$request->days,
+            'user_id' => (int)$request->user_id
         ]);
         $simcard = (new SimCardAction)->create($data);
         return $simcard;
     }
-    public function delete ($simCardId){
+    public function delete($simCardId)
+    {
         $simcard = SimCard::find($simCardId);
-        abort_unless((bool)$simcard,404,'simcard not found');
-        $simcard->delete();        
+        abort_unless((bool)$simcard, 404, 'simcard not found');
+        $simcard->delete();
         $activations = $simcard->activations;
-        foreach($activations as $activation){
+        foreach ($activations as $activation) {
             $activation->delete();
         }
         return $simcard;
+    }
+    public function search(Request $request)
+    {
+        $validated = $request->validate([
+            'keywords' => 'required|string'
+        ]);
+        $simCards = SimCard::where('number', 'like', '%' . $request->keywords . '%')->get();
+        return $simCards;
     }
 }
