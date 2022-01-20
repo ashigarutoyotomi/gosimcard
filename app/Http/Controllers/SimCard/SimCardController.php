cf3fd5a678d8241c5ab3c20b808b1735539bbfa7
abe codeman <abecodeman@gmail.com>
2022-01-20 13:10:05 +0600

    final fixes

--- app/Http/Controllers/SimCard/SimCardController.php
+++ app/Http/Controllers/SimCard/SimCardController.php
@@ -21,38 +21,22 @@ class SimCardController extends Controller
         $simCardsQuery = SimCard::query();
         if (!empty($request->get('keywords'))) {
             $simCardsQuery->where('number', 'like', '%' . $request->get('keywords') . '%');
-        } else if (!empty($request->get('filters')) && empty(($request->get('keywords')))) {
-            if (!empty($filters['status'])) {
-                $simCardsQuery->where('status', $filters['status']);
-            }
-            if (!empty($filters['start_created_date'])) {
-                $simCardsQuery->where('created_at', '>=', $filters['start_created_at']);
-            }
-            if (!empty($filters['end_created_date'])) {
-                $simCardsQuery->where('created_at', '<', $filters['end_created_'], '<=', $filters['end_created_date']);
-            }
-            $simCards = $simCardsQuery->get();
-            return $simCards;
-        } elseif (!empty($request->get('filters')) && (!empty($request->get('keywords')))) {
-            if (!empty($filters['status'])) {
-                $simCardsQuery->where('status', $filters['status']);
-            }
-            if (!empty($filters['start_created_date'])) {
-                $simCardsQuery->where('created_at', '>=', $filters['start_created_at']);
-            }
-            if (!empty($filters['end_created_date'])) {
-                $simCardsQuery->where('created_at', '<', $filters['end_created_'], '<=', $filters['end_created_date']);
-            }
-            $simCardsQuery->where('number', 'like', '%' . $request->get('keywords') . '%');
-
-            $simCards = $simCardsQuery->get();
-            return $simCards;
-        } else {
-            $simCards = SimCard::all();
-            $simCards = $simCardsQuery->get();
-            return $simCards;
         }
+        if (!empty($request->get('filters'))) {
+            if (!empty($filters['status'])) {
+                $simCardsQuery->where('status', $filters['status']);
+            }
+            if (!empty($filters['start_created_date'])) {
+                $simCardsQuery->where('created_at', '>=', $filters['start_created_at']);
+            }
+            if (!empty($filters['end_created_date'])) {
+                $simCardsQuery->where('created_at', '<', $filters['end_created_'], '<=', $filters['end_created_date']);
+            }
+        }
+        $simCards = $simCardsQuery->get();
+        return $simCards;
     }
+
     public function show($simCardId)
     {
         $simcard = SimCard::find($simCardId);
