d226f17c10a49227eac3dcff7a894d9f02ee26f1
abe codeman <abecodeman@gmail.com>
2022-01-24 17:06:29 +0600

    fixed and debugged

--- app/Domains/SimCard/DTO/SimCardDTO/CreateSimCardActivationData.php
+++ app/Domains/SimCard/DTO/SimCardDTO/CreateSimCardActivationData.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Domains\SimCard\DTO\SimCardDTO;
 
 use App\Http\Requests\SimCard\SimCardActivationRequest;
@@ -7,20 +8,23 @@ use Spatie\DataTransferObject\DataTransferObject;
 class CreateSimCardActivationData extends DataTransferObject
 {
     public int $available_days;
-    public int $end_date;
-    public int $start_date;
-    public int $user_id;
-    public int $sim_card_id;    
+    public string $end_date;
+    public string $start_date;
+    public ?int $user_id;
+    public int $sim_card_id;
+    public ?string $number;
+    public ?int $status;
 
-    public static function fromRequest(SimCardActivationRequest $request) : CreateSimCardActivationData
+    public static function fromRequest(SimCardActivationRequest $request): CreateSimCardActivationData
     {
         $data = [
-            'end_date' => (int)$request->end_date,
-            'start_date' => (int) $request->start_date,
-            'user_id'=>(int)$request->user_id,
-            'available_days'=>(int)$request->available_days,
-            'sim_card_id'=>$request->sim_card_id
-        ];        
+            'end_date' => $request->end_date,
+            'start_date' =>  $request->start_date,
+            'user_id' => $request->user_id,
+            'available_days' => $request->available_days,
+            'sim_card_id' => $request->sim_card_id,
+            'number' => $request->number,
+        ];
         return new self($data);
     }
 }
