bd84f49d67161278b24a09f5496fff5b7e22edb0
abe codeman <abecodeman@gmail.com>
2022-01-24 14:46:03 +0600

    tested n fixed

--- app/Domains/SimCard/DTO/SimCardDTO/CreateSimCardData.php
+++ app/Domains/SimCard/DTO/SimCardDTO/CreateSimCardData.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Domains\SimCard\DTO\SimCardDTO;
 
 use App\Http\Requests\SimCard\SimCardRequest;
@@ -6,19 +7,19 @@ use Spatie\DataTransferObject\DataTransferObject;
 
 class CreateSimCardData extends DataTransferObject
 {
-    public int $user_id;
+    public ?int $user_id;
     public string $number;
-    public int $days;
-    public int $status;
+    public ?int $days;
+    public ?int $status;
 
-    public static function fromRequest(SimCardRequest $request) : CreateSimCardData
+    public static function fromRequest(SimCardRequest $request): CreateSimCardData
     {
         $data = [
             'number' => $request->number,
-            'days' => (int) $request->days,
-            'user_id'=>(int)$request->user_id,
-            'status'=>(int)$request->status
-        ];        
+            'days' =>  $request->days,
+            'user_id' => $request->user_id,
+            'status' => $request->status
+        ];
         return new self($data);
     }
 }
--- database/migrations/2022_01_12_072223_create_simcards_table.php
+++ database/migrations/2022_01_12_072223_create_simcards_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use App\Domains\SimCard\Models\SimCard;
+
 class CreateSimcardsTable extends Migration
 {
     /**
@@ -16,13 +17,10 @@ class CreateSimcardsTable extends Migration
         Schema::create('simcards', function (Blueprint $table) {
             $table->id();
             $table->string('number')->unique();
-            $table->integer('status')->default(SimCard::STATUS_NEW);
+            $table->integer('status')->nullable();
             $table->integer('days')->default(0);
-
             $table->unsignedBigInteger('user_id')->nullable();
-
             $table->timestamps();
-
             $table->foreign('user_id')
                 ->references('id')
                 ->on('users');
